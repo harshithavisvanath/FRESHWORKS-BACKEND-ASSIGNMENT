ef6c98863aed66a3b8df90457a04ba39bb74e1ca
HARSHITHA  harshi03 <37268971+harshithavisvanath@users.noreply.github.com>
2020-12-01 21:31:01 +0530

    Update crdoperations.py

--- crdoperations.py
+++ crdoperations.py
@@ -40,19 +40,6 @@ def read(key):
 
 
 
-def delete(key):
-    if key not in f:
-        print("Please enter a valid key, Not in database") 
-    else:
-        b=f[key]
-        if b[1]!=0:
-            if time.time()<b[1]: 
-                del f[key]
-                print("key is successfully deleted")
-            else:
-                print("time-to-live of",key,"has expired") 
-        else:
-            del f[key]
-            print("key is successfully deleted")
+
 
 
